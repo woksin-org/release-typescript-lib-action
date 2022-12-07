2276684e44255c01be53d50c1f9c9ec4a567711d
woksin <sindrewilting@gmail.com>
2022-12-07 13:26:22 +0100

    Update dependencies

--- Source/for_something/a_test.ts
+++ Source/for_something/a_test.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-describe('a test', () => {
+import { describeThis } from '@dolittle/typescript.testing';
+
+describeThis(__filename, () => {
     it('should be true', () => true.should.be.true);
 });
